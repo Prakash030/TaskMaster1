d22e1835e4112a06e97637b2dadf159ec3b901a5
Prakash kumar nayak <106471406+Prakash030@users.noreply.github.com>
2023-10-12 15:36:20 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,11 @@ const list = require('./Routes/list')
 const cors = require('cors')
 
 app.use(express.json());
-app.use(cors())
+app.use(cors(
+    origin: ["https://task-master1.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+))
 
 app.get('/',(req,res)=>{
     res.send('Hello World');
